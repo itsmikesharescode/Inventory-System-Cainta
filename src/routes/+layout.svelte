0ff8c1ebdc94edfd7714e1f77ff4ab08a0bc2ea0
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 00:34:33 +0800

    add view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
   import '../app.css';
+  import { onNavigate } from '$app/navigation';
   const { data, children } = $props();
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 {@render children()}
