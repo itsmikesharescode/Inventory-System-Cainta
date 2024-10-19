06361ddf1fb29efb9911fe8501b5c2b77d18f43e
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-19 23:18:24 +0800

    add login layout

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
 </script>
 
-<div class=""></div>
+<div class="grid min-h-screen grid-cols-[2fr,1fr]">
+  <div class="flex flex-col items-center justify-center">
+    <img src="/favicon.png" alt="system logo" class="w-[200px]" />
+    <h1
+      class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+    >
+      ONE CAINTA COLLEGE BORROWING AND INVENTORY SYSTEM
+    </h1>
+  </div>
+  <div class="flex items-center justify-center border-l-2">
+    <h1 class="scroll-m-20 text-2xl font-semibold tracking-tight">Log in</h1>
+  </div>
+</div>
---
+++ src/routes/admin/(index)/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="grid min-h-screen grid-cols-[1fr,100px]">
+  <div class="bg-red-500">s</div>
+  <div class="">asdasd</div>
+</div>
