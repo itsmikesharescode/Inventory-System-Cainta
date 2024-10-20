e5653f316b354b5221dbbffccfd56b087a98491c
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 13:32:36 +0800

    add reset

--- src/routes/admin/items/_components/AddItem/AddItemModal.svelte
+++ src/routes/admin/items/_components/AddItem/AddItemModal.svelte
@@ -48,7 +48,10 @@
   <AlertDialog.Content class="p-0">
     <ScrollArea class="max-h-screen md:max-h-[80dvh]">
       <button
-        onclick={() => (open = false)}
+        onclick={() => {
+          open = false;
+          form.reset();
+        }}
         class="absolute right-4 top-4 z-30 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
       >
         <X class="h-4 w-4" />
