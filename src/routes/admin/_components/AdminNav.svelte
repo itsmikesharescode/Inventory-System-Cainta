8b1c0f03f200fdf223bcb50cf5f6ec90376b0a73
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 08:51:18 +0800

    add admin nav and admin profile

---
+++ src/routes/admin/_components/AdminNav.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import AdminProfile from './AdminProfile/AdminProfile.svelte';
+
+  interface Props {
+    children: Snippet;
+  }
+
+  const { children }: Props = $props();
+
+  const adminSiteMaps = [
+    {
+      name: 'Dashboard',
+      url: '/admin',
+      subRoutes: false
+    },
+    {
+      name: 'Transactions',
+      url: false,
+      subRoutes: [
+        {
+          name: 'Reservations',
+          url: '/admin/reservations'
+        },
+        {
+          name: 'New',
+          url: '/admin/new'
+        },
+        {
+          name: 'Borrowed Items',
+          url: '/admin/borrowed-items'
+        },
+        {
+          name: 'Returned Items',
+          url: '/admin/returned-items'
+        }
+      ]
+    },
+    {
+      name: 'Item',
+      url: '/admin/item',
+      subRoutes: false
+    },
+    {
+      name: 'Borrower',
+      url: '/admin/borrower',
+      subRoutes: false
+    }
+  ];
+</script>
+
+<nav class="flex items-center justify-end p-1.5">
+  <AdminProfile />
+</nav>
---
+++ src/routes/admin/_components/AdminProfile/AdminProfile.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import * as Popover from '$lib/components/ui/popover';
+  import * as Avatar from '$lib/components/ui/avatar';
+</script>
+
+<Popover.Root>
+  <Popover.Trigger>
+    <Avatar.Root>
+      <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+      <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+  </Popover.Trigger>
+  <Popover.Content>Place content for the popover here.</Popover.Content>
+</Popover.Root>
