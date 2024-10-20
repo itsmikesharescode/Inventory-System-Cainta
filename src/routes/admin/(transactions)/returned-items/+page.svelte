e370ef36192362cf6bf413916b73d9055abbe6b8
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 10:32:27 +0800

    update UI

---
+++ src/routes/admin/(transactions)/returned-items/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import GradualSpacing from '$lib/components/gen/GradualSpacing.svelte';
+</script>
+
+<div class="flex flex-col gap-5">
+  <div class="flex">
+    <GradualSpacing
+      class="font-display text-left text-4xl font-bold tracking-[-0.1em]  text-black   md:leading-[5rem]"
+      words="Returned Items"
+    />
+  </div>
+</div>
