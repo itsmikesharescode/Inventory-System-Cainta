4d5bf14a77e0a788367dde1ec6adc3a9f8b8aa44
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 19:38:37 +0800

    update icons

--- src/routes/admin/borrowers/_components/TeacherMenu.svelte
+++ src/routes/admin/borrowers/_components/TeacherMenu.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { FolderCog } from 'lucide-svelte';
+  import { UserCog } from 'lucide-svelte';
   import * as Menubar from '$lib/components/ui/menubar';
   import type { Infer, SuperValidated } from 'sveltekit-superforms';
   import { pushState } from '$app/navigation';
@@ -18,7 +18,7 @@
 <Menubar.Root preventScroll={true} class="max-w-fit border-0">
   <Menubar.Menu>
     <Menubar.Trigger class="max-w-fit cursor-pointer data-[state=open]:bg-inherit">
-      <FolderCog class="h-[20px] w-[20px]" />
+      <UserCog class="h-[20px] w-[20px]" />
     </Menubar.Trigger>
 
     <Menubar.Content>
