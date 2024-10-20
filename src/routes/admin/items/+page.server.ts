c28efccd9b1bd664389e83a88a0163004321730d
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 15:47:28 +0800

    update add item backend

--- src/routes/admin/items/+page.server.ts
+++ src/routes/admin/items/+page.server.ts
@@ -2,7 +2,7 @@ import { superValidate } from 'sveltekit-superforms';
 import type { Actions, PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { addItemSchema } from './_components/AddItem/schema';
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import { updateItemSchema } from './_components/UpdateItem/schema';
 
 export const load: PageServerLoad = async () => {
@@ -13,12 +13,30 @@ export const load: PageServerLoad = async () => {
 };
 
 export const actions: Actions = {
-  addItemEvent: async ({ locals: { supabase }, request }) => {
+  addItemEvent: async ({ locals: { supabase, user }, request }) => {
     const form = await superValidate(request, zod(addItemSchema));
-
+    if (!user) redirect(303, '/?error=no-session');
     if (!form.valid) return fail(400, { form });
 
-    console.log(form.data);
+    const { error } = await supabase.from('items_tb').insert([
+      {
+        admin_id: user.id,
+        device_id: form.data.deviceId,
+        model: form.data.model,
+        category: form.data.category,
+        type: form.data.type,
+        status: form.data.status,
+        mr: form.data.mr,
+        brand: form.data.brand,
+        quantity: form.data.quantity,
+        price: form.data.price,
+        description: form.data.description
+      }
+    ]);
+
+    if (error) return fail(401, { form, msg: error.message });
+
+    return { form, msg: 'Item added.' };
   },
 
   updateItemEvent: async ({ locals: { supabase }, request }) => {
