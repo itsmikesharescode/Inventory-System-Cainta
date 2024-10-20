326a121a5775489efde87fa94dbaf992f43788ed
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-20 19:32:06 +0800

    add update teacher moda

---
+++ src/routes/admin/borrowers/_components/TeacherMenu.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { FolderCog } from 'lucide-svelte';
+  import * as Menubar from '$lib/components/ui/menubar';
+  import type { Infer, SuperValidated } from 'sveltekit-superforms';
+  import { pushState } from '$app/navigation';
+
+  interface Props {
+    updateTeacherForm: SuperValidated<Infer<UpdateItemSchema>>;
+  }
+
+  const { updateTeacherForm }: Props = $props();
+
+  let showUpdateTeacher = $state(false);
+  let showDeleteTeacher = $state(false);
+  let showViewTeacber = $state(false);
+</script>
+
+<Menubar.Root preventScroll={true} class="max-w-fit border-0">
+  <Menubar.Menu>
+    <Menubar.Trigger class="max-w-fit cursor-pointer data-[state=open]:bg-inherit">
+      <FolderCog class="h-[20px] w-[20px]" />
+    </Menubar.Trigger>
+
+    <Menubar.Content>
+      <Menubar.Item
+        onclick={() => {
+          showViewTeacber = true;
+          /* pushState(`/admin/items/${item.id}`, {}); */
+        }}
+      >
+        View
+        <Menubar.Shortcut>⌘V</Menubar.Shortcut>
+      </Menubar.Item>
+
+      <Menubar.Separator />
+      <Menubar.Item onclick={() => (showUpdateTeacher = true)}>
+        Update
+        <Menubar.Shortcut>⌘U</Menubar.Shortcut>
+      </Menubar.Item>
+      <Menubar.Separator />
+      <Menubar.Item onclick={() => (showDeleteTeacher = true)}
+        >Delete
+        <Menubar.Shortcut>⌘D</Menubar.Shortcut>
+      </Menubar.Item>
+    </Menubar.Content>
+  </Menubar.Menu>
+</Menubar.Root>
