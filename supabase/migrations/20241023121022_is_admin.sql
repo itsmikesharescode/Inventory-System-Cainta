77399b21976cb6318a0fccf58a6bce77f9968987
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-23 20:47:20 +0800

    add supabase cli

---
+++ supabase/migrations/20241023121022_is_admin.sql
@@ -0,0 +1,15 @@
+create or replace function is_admin() 
+returns boolean 
+as $$
+begin
+  return exists(
+    select 1 
+    from role_tb 
+    where user_id = auth.uid() 
+      and role = 'admin'
+  );
+end;
+$$ 
+language plpgsql 
+security definer 
+set search_path = public;
