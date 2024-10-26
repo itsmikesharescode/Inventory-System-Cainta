009037c9765d0a51396d1f406ff72ec3911b250b
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-26 23:35:52 +0800

    add table ui

--- src/routes/admin/(transactions)/borrowed-items/+page.svelte
+++ src/routes/admin/(transactions)/borrowed-items/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import GradualSpacing from '$lib/components/gen/GradualSpacing.svelte';
+  import * as Table from '$lib/components/ui/table';
+  import BorrowedMenu from './_components/BorrowedMenu.svelte';
 </script>
 
 <div class="flex flex-col gap-5">
@@ -9,4 +11,30 @@
       words="Borrowed Items"
     />
   </div>
+
+  <Table.Root>
+    <Table.Caption>A list of your recent invoices.</Table.Caption>
+    <Table.Header>
+      <Table.Row>
+        <Table.Head class="w-[100px]"></Table.Head>
+        <Table.Head>Borrower Name</Table.Head>
+        <Table.Head>Borrowed Date</Table.Head>
+        <Table.Head>Items Borrowed</Table.Head>
+        <Table.Head>Room</Table.Head>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {#each Array(20) as _}
+        <Table.Row>
+          <Table.Cell>
+            <BorrowedMenu />
+          </Table.Cell>
+          <Table.Cell>asdasd</Table.Cell>
+          <Table.Cell>asdasd</Table.Cell>
+          <Table.Cell>asdasd</Table.Cell>
+          <Table.Cell>asdasd</Table.Cell>
+        </Table.Row>
+      {/each}
+    </Table.Body>
+  </Table.Root>
 </div>
---
+++ src/routes/admin/(transactions)/borrowed-items/_components/BorrowedMenu.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { AlignJustify } from 'lucide-svelte';
+  import * as Menubar from '$lib/components/ui/menubar';
+  import type { Infer, SuperValidated } from 'sveltekit-superforms';
+  import { pushState } from '$app/navigation';
+  import type { AdminLayout } from '$lib/types/admin/adminLayout.types';
+
+  /* interface Props {
+    updateReservationForm: SuperValidated<Infer<UpdateReservationSchema>>;
+    reservation: AdminLayout['reservations'][number];
+  }
+
+  const { ...props }: Props = $props(); */
+
+  let showUpReservation = $state(false);
+  let showDelReservation = $state(false);
+  let showViewReservation = $state(false);
+</script>
+
+<Menubar.Root preventScroll={true} class="max-w-fit border-0">
+  <Menubar.Menu>
+    <Menubar.Trigger class="max-w-fit cursor-pointer data-[state=open]:bg-inherit">
+      <AlignJustify class="h-[20px] w-[20px]" />
+    </Menubar.Trigger>
+
+    <Menubar.Content>
+      <Menubar.Item
+        onclick={() => {
+          showViewReservation = true;
+          /* pushState(`/admin/reservations/${props.reservation.id}`, {}); */
+        }}
+      >
+        View
+        <Menubar.Shortcut>⌘V</Menubar.Shortcut>
+      </Menubar.Item>
+
+      <Menubar.Separator />
+      <Menubar.Item onclick={() => (showUpReservation = true)}>
+        Update
+        <Menubar.Shortcut>⌘U</Menubar.Shortcut>
+      </Menubar.Item>
+      <Menubar.Separator />
+      <Menubar.Item onclick={() => (showDelReservation = true)}
+        >Delete
+        <Menubar.Shortcut>⌘D</Menubar.Shortcut>
+      </Menubar.Item>
+    </Menubar.Content>
+  </Menubar.Menu>
+</Menubar.Root>
