6fe2d4db92cd80529cf67ebaa0f4f5948e76c156
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-27 12:14:51 +0800

    update layout sql

--- supabase/migrations/20241023131218_admin_layout.sql
+++ supabase/migrations/20241023131218_admin_layout.sql
@@ -4,6 +4,7 @@ declare
   item_data jsonb;
   teacher_data jsonb;
   reservation_data jsonb;
+  borrowed_data jsonb;
 begin
 
   select json_agg(row_to_json(itd)) into item_data
@@ -15,10 +16,14 @@ begin
   select json_agg(row_to_json(rtd)) into reservation_data
   from (select * from public.reservations_tb order by created_at asc) as rtd;
 
+  select json_agg(row_to_json(btd)) into borrowed_data
+  from (select * from public.borrowed_items_tb order by created_at asc) as btd;
+
   return jsonb_build_object(
     'items', coalesce(item_data, '[]'::jsonb),
     'teachers', coalesce(teacher_data, '[]'::jsonb),
-    'reservations', coalesce(reservation_data, '[]'::jsonb)
+    'reservations', coalesce(reservation_data, '[]'::jsonb),
+    'borrowed_items', coalesce(borrowed_data, '[]'::jsonb)
   );
 end;
 $$ language plpgsql set search_path = '';
