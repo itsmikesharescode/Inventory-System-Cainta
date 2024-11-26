fb541ff26c634c57845186331b664edb0e019441
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-26 20:33:22 +0800

    update things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,19 @@
-<script>
+<script lang="ts">
   import '../app.css';
   import { invalidate, onNavigate } from '$app/navigation';
   import { Toaster } from '$lib/components/ui/sonner';
   import { onMount } from 'svelte';
-  import { fromUserState, initUserState } from '$lib/runes/userState.svelte';
-  import { fromSupabaseState, initSupabaseState } from '$lib/runes/supabaseState.svelte';
 
   const { data: clientSb, children } = $props();
 
-  onMount(() => {
+  $effect(() => {
     const { data } = clientSb.supabase.auth.onAuthStateChange((_, newSession) => {
       if (newSession?.expires_at !== clientSb.session?.expires_at) {
         invalidate('supabase:auth');
       }
     });
 
-    return () => data.subscription.unsubscribe();
+    return data.subscription.unsubscribe();
   });
 
   onNavigate((navigation) => {
